81e75348136cf67b46b34dad169def8748f30ed6
twpDiogenes <twisepigeon@gmail.com>
2022-08-31 14:26:50 +0000

    feat:added signup with firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { logOut, signupWithEmailAndPassword } from './firebase.js'
 
 function App() {
+
+  const handle = async (event)=>{
+    event.preventDefault()
+    console.log('got submitted');
+    signupWithEmailAndPassword("zozozozeph@gmail.com", "007#feartheDREAD")
+  }
+
 
   return (
     <>
     <p className=" bg-red-400 text-center">Welcome to WARDLE</p>
+    <form onSubmit={handle} action="" method="post">
+      <input className=" outline-orange-500" placeholder="email" type="email" name="mail" id="" />
+      <input className=" outline-orange-800" placeholder="pass" type="password" name="pass" id="" />
+      <button  type="submit">
+        Submit
+      </button>
+    </form>
     </>
   )
 }
